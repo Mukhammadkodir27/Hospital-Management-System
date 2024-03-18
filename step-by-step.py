7223a09f5a89eb92a3436622e5fad86c8cd1e54c
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-03-18 12:48:52 +0100

    Update step-by-step.py

--- step-by-step.py
+++ step-by-step.py
@@ -7,5 +7,14 @@
 #notice (my-venv) has to appear in your terminal, it means virtual environment is activated
 (my-venv) > pip install django 
 
-django-admin startproject Hospital
+>django-admin startproject Hospital
+#success
 
+>django-admin startapp app
+
+add "app",   in installed apps from settings.py
+
+>python manage.py migrate
+>python manage.py runserver
+
+# success
