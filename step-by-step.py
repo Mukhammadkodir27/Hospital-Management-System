acb5b45ff3e1278b0ce14ba46e01e3279b99bf12
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-03-18 12:31:01 +0100

    Update step-by-step.py

--- step-by-step.py
+++ step-by-step.py
@@ -1,1 +1,11 @@
-problem encountered
+# If you encounter an error with your virtual environment, run Powershell as an administrator and use below command
+# Set-ExecutionPolicy RemoteSigned
+
+# to create our virtual environment
+> python -m venv my-venv 
+
+(my-venv) > pip install django 
+
+django-admin startproject Hospital
+
+
